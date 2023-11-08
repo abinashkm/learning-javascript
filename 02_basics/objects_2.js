01292ad67b0bc6e4e6bca53bc96197ca27dedeed
Abinash Mohanty <abinashkummar333@gmail.com>
2023-11-08 22:10:27 +0530

    about objects_2
    singleton object, nsted objects, array of objects, combine and methods for key, value

---
+++ 02_basics/objects_2.js
@@ -0,0 +1,50 @@
+const space = new Object() // this is the method to create a singleton object.
+
+const universe = {} // this is another way to declare object.
+
+universe.planets = 12345
+universe.world = "earth"
+
+console.log(universe);  // { planets: 12345, world: 'earth' }
+
+/** NESTING OF OBJECTS */
+
+const bluePlanet = {
+    spaceId: 'ozone',
+    nature: {
+        forest: {
+            trees: 333333333330
+        }
+    }
+}
+
+console.log(bluePlanet.nature.forest.trees); // in nested objects u have to specify each individual object from which u want ur value.
+
+/** COMBINE OBJECTS */
+let obj1 = {a: 1, b: 2}
+let obj2 = {c: 3, d: 4}
+let combinedObj = Object.assign({}, obj1 , obj2)
+console.log(combinedObj);
+//{ a: 1, b: 2, c: 3, d: 4 }
+// So here concat or push dose not work. Only spread and assign works.
+// most of the time we will use spread method.
+
+
+/** ARRAY OF OBJECTS */
+
+const arr1 = [
+    {name:'John',
+    hobbies:'art'},
+    {name:'John',
+    hobbies:'art'},
+    {name:'John',
+    hobbies:'art'}
+]
+console.log(arr1[0].hobbies); // to read objects inside array.
+
+// NOW THERE ARE FEW IMPORTANT METHODS THAT WE USE SPECIALLY IN DATABASE.
+
+console.log(Object.keys(universe)); // to read the keys
+console.log(Object.values(universe)); // to get values from an object.
+console.log(Object.entries(universe)) // to get key value pairs in an array.
+console.log(universe.hasOwnProperty('world')); // to check if the required key exists or not.
