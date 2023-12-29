9f9800266a4514a901a5667b89bd54e3cca390a3
Abinash Mohanty <abinashkummar333@gmail.com>
2023-12-29 23:13:02 +0530

    updated

---
+++ 04_basics/promises.js
@@ -0,0 +1,120 @@
+// Generally the use promise function we have to:
+// 1. Create a new Promise object
+// 2. Pass in an executor function that gets called immediately when the Promise is instantiated
+// The executor function has two parameters, resolve and reject. These are functions that get passed into the executor function by whoever creates the Promise.
+
+
+
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+// THIS IS ANOTHER WAY WE CAN USE THIS.
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+
+// Below, we are extracting some data from the function.
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+
+// Here, we have applied error too so the syntax will work like this.
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+// this process is known as chaining. 
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+// There is another method known as async, where we use await.
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # learning-javascript
  In this repo we will be learning JAVASCRIPT from the start to end.
+ This is my personal JavaScript notes. It may not have all the required topics.
 
-Before going ahead to learn javascript we should keep a clear a goal for its use. Like making a custom website or creating a SAAS website.
+ 
